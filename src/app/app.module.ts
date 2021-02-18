c314c29e6eb22563c0d65ef37ca331fa40906ef7
Danny Escalante <danny.escalante@multicaja.cl>
2021-02-18 11:00:50 -0300

    modificacion final, api paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './pais/shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
